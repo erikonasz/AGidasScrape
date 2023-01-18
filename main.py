1c057d16970815e296a13a88a6d35193c6129c3c
erikonasz <arcikauskas.erikas@gmail.com>
2023-01-18 19:25:22 +0200

    Added liter, fuel_type, year, transmission by scraping each car description and stripping it to save time.

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup
 import csv
 
-
 driver = webdriver.Chrome(executable_path=ChromeDriverManager().install())
 driver.get("https://autogidas.lt/en/skelbimai/automobiliai/porsche/")
 
@@ -21,13 +20,28 @@ total_cars = 0
 
 while True:
     soup = BeautifulSoup(driver.page_source, 'html.parser')
-
     allcars = soup.find_all('article', class_='list-item')
 
     for car in allcars:
         title = car.find('h2', class_='item-title').text
         price = car.find('div', class_='item-price').text
-        cars.append({'title': title, 'price': price})
+
+        description = car.find('div', class_='item-description').text
+
+        description_list = description.split(',')
+
+        liter = description_list[0].strip()
+        fuel_type = description_list[1].strip()
+        year = description_list[2].strip()
+        transmission = description_list[3].strip()
+        try:
+            engine = description_list[4].strip()
+        except:
+            print("Engine info (4 list) not found.")
+
+
+        cars.append({'title': title, 'price': price, 'liter': liter, 'fuel_type': fuel_type, 'year': year,
+                     'transmission': transmission, 'engine': engine})
         total_cars += 1
     next_page_button = driver.find_elements(By.XPATH, ("//div[@class='next-page-inner']"))
     if not next_page_button:
@@ -40,11 +54,11 @@ print("Total number of cars: ", len(cars))
 print("Cars list: ", cars)
 
 with open('cars.csv', mode='w', newline='') as csv_file:
-    fieldnames = ['title', 'price']
+    fieldnames = ['title', 'price', 'liter', 'fuel_type', 'year', 'transmission', 'engine']
     wrt = csv.DictWriter(csv_file, fieldnames=fieldnames)
     wrt.writeheader()
 
     for car in cars:
         car['title'] = car['title'].strip()
-        car['title'] = car['title'].strip()
+        car['price'] = car['price'].strip()
         wrt.writerow(car)
