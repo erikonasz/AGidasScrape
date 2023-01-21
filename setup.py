8cda5db060cc291f3aad3130ec164a343f151cee
erikonasz <arcikauskas.erikas@gmail.com>
2023-01-21 23:42:14 +0200

    Removed re and csv from setup.py as it is not needed.

--- setup.py
+++ setup.py
@@ -1,18 +1,21 @@
 from setuptools import setup, find_packages
 
+
 setup(
     name='AGidasScraper',
-    version='0.1',
-    packages=['AGidasScraper'],
+    version='0.3',
+    packages=find_packages(),
     install_requires=[
         'beautifulsoup4',
         'webdriver-manager',
-        're',
-        'csv'
     ],
     entry_points={
         'console_scripts': [
             'AGidasScraper = AGidasScraper.ascrape:main'
         ]
-    }
+    },
+    author = "erikonasz",
+    author_email = "erikuzas123@gmail.com",
+    description = "Autogidas scraper",
+    url = "https://github.com/erikonasz/AGidasScrape"
 )
